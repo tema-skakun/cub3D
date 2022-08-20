457b377957af1f6e505fc5ea10318a931409cd1f
Mohamed Meredith <mmeredit@at-n8.msk.21-school.ru>
2022-08-20 15:23:15 +0300

    full 2d raycasting

--- src/execute.c
+++ src/execute.c
@@ -15,24 +15,17 @@ int	close_esc(int keycode, t_game *game)
 	return (0);
 }
 
-int	move_all(t_game *game, double x, double y, int keycode)
+int	move_all(t_game *game, double x, double y)
 {
-	printf ("x = %f y = %f\n", game->info->player_pos_x / 32, game->info->player_pos_y / 32);
-	if (game->map[(int)(game->info->player_pos_y / 32 + y)][(int)(game->info->player_pos_x / 32 + x)] == '1')
-		return (1);
-
-	printf ("sin = %f\n", sin(game->info->view));
-	printf ("cos = %f\n", cos(game->info->view));
-	printf ("radian = %f\n", game->info->view);
-	if (keycode == 13 || keycode == 1)
+	if (x == y)
 	{
-		game->info->player_pos_x = game->info->player_pos_x + y * 32 * (sin(game->info->view));
-		game->info->player_pos_y = game->info->player_pos_y + x * 32 * (cos(game->info->view));
+		game->info->player_pos_x += y * cos(game->info->view + 30 * RADIAN);
+		game->info->player_pos_y += x * sin(game->info->view + 30 * RADIAN);
 	}
 	else
 	{
-		game->info->player_pos_x = game->info->player_pos_x + y * 32 * (cos(game->info->view - 30 * RADIAN));
-		game->info->player_pos_y = game->info->player_pos_y + x * 32 * (sin(game->info->view - 30 * RADIAN));
+		game->info->player_pos_x += y * cos(game->info->view - 60 * RADIAN);
+		game->info->player_pos_y += x * sin(game->info->view + 120 * RADIAN);
 	}
 	set_map(game);
 	return (1);
@@ -40,35 +33,25 @@ int	move_all(t_game *game, double x, double y, int keycode)
 
 int	look(t_game *game, double x)
 {
-	// if (game->info->view == 360 * RADIAN)
-	// 	game->info->view = 0;
-	game->info->view = (game->info->view / RADIAN + x) * RADIAN;
+	game->info->view += x*RADIAN;
 	set_map(game);
 	return(1);
 }
 
 int move(int keycode, t_game *game){
-	printf("I AM MOVE! keycode = %d\n", keycode);
 	close_esc(keycode, game);
 	if (keycode == 13) // w
-		move_all(game, 0.4, -0.4, keycode);
+		move_all(game, 0.4, 0.4);
 	else if (keycode == 0) // a
-		move_all(game, -0.4, 0.4, keycode);
+		move_all(game, -0.4, 0.4);
 	else if (keycode == 1) // s
-		move_all(game, -0.4, 0.4, keycode);
+		move_all(game, -0.4, -0.4);
 	else if (keycode == 2) // d
-		move_all(game, 0.4, -0.4, keycode);
-	else if (keycode == 123)
+		move_all(game, 0.4, -0.4);
+	else if (keycode == 123) // l
 		look(game, -3);
-	else if (keycode == 124)
+	else if (keycode == 124) // r
 		look(game, 3);
-	// else if (keycode == 124)
-	// 	right();
-	// if (!game->game_over)
-	// {
-	// 	mlx_clear_window(game->libx->mlx, game->libx->win);
-	// 	last_img(game, 0, 0);
-	// }
 	return (0);
 }
 
@@ -93,8 +76,6 @@ void find_pos_hero(char **map, t_info *info){
 		j = 0;
 		i++;
 	}
-	info->player_pos_x *= 32;
-	info->player_pos_y *= 32;
 }
 
 int argb_to_int(int a, int r, int g, int b){
@@ -108,7 +89,7 @@ void	draw_floor_ceilling(t_game *game)
 	int				color;
 	unsigned int	*tmp;
 
-	printf ("x = %d y = %d\n", game->x, game->y);
+	// printf ("x = %d y = %d\n", game->x, game->y);
 	img.ptr = mlx_new_image(game->vars->mlx, game->y * 32, game->x * 32);
 	img.addr = mlx_get_data_addr(img.ptr, &img.bits_per_pixel, &img.size_line, &img.endian);
 	tmp = (unsigned int *)img.addr;
@@ -143,7 +124,6 @@ void	draw_country(t_game *game, double x, double y, double length, int color)
 	x += game->y * 16; // координата центра карты + координата точки по (y) или (x) для массива
 	y += game->x * 16; // координата центра карты + координата точки по (x) или (y) для массива
 	// printf ("x = %f  y = %f k = %f\n", x, y, k);
-	// d = z
 	up = game->y * 16 + k * 32;
 	while (k > 0)
 	{
@@ -154,39 +134,6 @@ void	draw_country(t_game *game, double x, double y, double length, int color)
 		// x++;
 	}
 	// while()
-}
-
-void	set_country(t_game *game)
-{
-	double i = game->info->player_pos_y / 32;
-	double j = game->info->player_pos_x / 32;
-	int color = argb_to_int(0, 20, 255, 20);
-	char	**map;
-	double	pixel;
-
-	map = game->map;
-
-	printf ("map = %c\n", map[(int)i][(int)j]);
-	pixel = 0;
-	double counter = game->info->view / RADIAN;
-	while (counter < game->info->view / RADIAN + 60)
-	{
-		while (map[(int)i][(int)j] && map[(int)i][(int)j] != '1' && i != 0)
-		{
-			// mlx_pixel_put(game->vars->mlx, game->vars->win, j * 32, i * 32, color);
-			// mlx_put_image_to_window(game->vars->mlx, game->vars->win, img.ptr, j * 32, i * 32);
-			i = game->info->player_pos_y / 32 + pixel * sin(RADIAN * counter);
-			j = game->info->player_pos_x / 32 + pixel * cos(RADIAN * counter);
-			pixel += 0.05;
-			// printf ("x = %f  y = %f\n", i, j);
-		}
-		draw_country(game, i, j, pixel, color);
-		// printf ("pixel = %f\n", pixel);
-		pixel = 0;
-		i = game->info->player_pos_y / 32;
-		j = game->info->player_pos_x / 32;
-		counter += (double) 60 / ((game->y - 1) * 32);
-	}
 }
 
 void	set_pixel_minimap(int color, t_game *game, int i, int j)
@@ -229,13 +176,12 @@ void	set_minimap(t_game *game)
 		i++;
 	}
 	color = argb_to_int(0, 255, 1, 1);
-	// set_pixel_minimap(color, game, game->info->player_pos_y * 2, game->info->player_pos_x);
 }
 
 void	some_raycasting(t_game *game)
 {
-	double i = game->info->player_pos_y / 32;
-	double j = game->info->player_pos_x / 32;
+	double i = game->info->player_pos_y;
+	double j = game->info->player_pos_x;
 	int color = argb_to_int(0, 20, 255, 20);
 	char	**map;
 	double	pixel;
@@ -245,7 +191,7 @@ void	some_raycasting(t_game *game)
 	map = game->map;
 
 	// (game->y - 1) * 32 / 60 - количество лучей которые нужно отбросить. // 
-	printf ("map = %c\n", map[(int)i][(int)j]);
+	// printf ("map = %c\n", map[(int)i][(int)j]);
 	pixel = 0;
 	double counter = game->info->view / RADIAN;
 	while (counter < game->info->view / RADIAN + 60)
@@ -253,25 +199,21 @@ void	some_raycasting(t_game *game)
 		while (map[(int)i][(int)j] && map[(int)i][(int)j] != '1' && i != 0)
 		{
 			mlx_pixel_put(game->vars->mlx, game->vars->win, j * 32, (i + game->x) * 32, color);
-			// mlx_put_image_to_window(game->vars->mlx, game->vars->win, img.ptr, j * 32, i * 32);
-			i = game->info->player_pos_y / 32 + pixel * sin(RADIAN * counter);
-			j = game->info->player_pos_x / 32 + pixel * cos(RADIAN * counter);
+			i = game->info->player_pos_y + pixel * sin(RADIAN * counter);
+			j = game->info->player_pos_x + pixel * cos(RADIAN * counter);
 			pixel += 0.05;
-			// printf ("x = %f  y = %f\n", i, j);
 		}
-		// printf ("pixel = %f\n", pixel);
+		draw_country(game, i, j, pixel, color);
 		pixel = 0;
-		i = (game->info->player_pos_y) / 32;
-		j = game->info->player_pos_x / 32;
+		i = game->info->player_pos_y;
+		j = game->info->player_pos_x;
 		counter += (double) 60 / ((game->y - 1) * 32);
-		// printf ("counter = %f\n", counter);
 	}
 }
 
 void 	set_map(t_game *game)
 {
 	draw_floor_ceilling(game);
-	set_country(game);
 	set_minimap(game);
 	some_raycasting(game);
 }
