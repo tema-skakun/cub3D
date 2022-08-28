f0a64474ccce0dff16588e8ef8f96eba7740bf52
Mohamed Meredith <mmeredit@at-p2.msk.21-school.ru>
2022-08-28 16:27:12 +0300

    maybe this formula

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: mmeredit <mmeredit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 15:23:57 by mmeredit          #+#    #+#             */
-/*   Updated: 2022/08/28 14:28:45 by mmeredit         ###   ########.fr       */
+/*   Updated: 2022/08/28 16:26:39 by mmeredit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,13 @@ void	full_raycasting(t_game *game, double *i, double *j, double degree)
 	}
 }
 
+double	ft_abs(double number)
+{
+	if (number < 0)
+		return (-number);
+	return (number);
+}
+
 void	draw_country(t_game *game, double x, double y, double length, int color, int c, double degree)
 {
 	double		k;
@@ -88,6 +95,9 @@ void	draw_country(t_game *game, double x, double y, double length, int color, in
 	(void)	degree;
 
 	k = length * 32;
+	// k = length * 32 * sin(ft_abs(PI - ft_abs(game->info->view - PI / 2) - degree));
+	// k = game->x * 32 / k;
+	printf("k = %f\n", k);
 	// dx = length * 32 * cos(degree);
 	// dy = length * 32 * sin(degree);
 	// k = dx * cos(from_zero_to_pi(game->info->view - degree)) + dy * sin(from_zero_to_pi(game->info->view - degree));
@@ -104,13 +114,6 @@ void	draw_country(t_game *game, double x, double y, double length, int color, in
 	}
 	// while()
 }
-
-// double	abs(double number)
-// {
-// 	if (number < 0)
-// 		return (-number);
-// 	return (number);
-// }
 
 int	define_side(char **map, double i, double j)
 {
@@ -149,12 +152,12 @@ void	some_raycasting(t_game *game)
 	{
 		while (map[(int)i][(int)j] && (map[(int)i][(int)j] != '1'))
 		{
-			mlx_pixel_put(game->vars->mlx, game->vars->win, (int)j * 32, (int)(i + game->x) * 32, color);
+			mlx_pixel_put(game->vars->mlx, game->vars->win, j * 32, (i + game->x) * 32, color);
 			full_raycasting(game, &i, &j, from_zero_to_pi(fov1));
 			if (!define_side(map, i, j)) // доп проверка для попадание в стену
 				break;
 		}
-		mlx_pixel_put(game->vars->mlx, game->vars->win, (int)j * 32, (int)(i + game->x) * 32, color);
+		mlx_pixel_put(game->vars->mlx, game->vars->win, j * 32, (i + game->x) * 32, color);
 		pixel = sqrt(pow(i - game->info->player_pos_y, 2) + pow(j - game->info->player_pos_x, 2));
 		draw_country(game, i, j, pixel, color, k++, from_zero_to_pi(fov1));
 		// printf("k = %d\n", k);
